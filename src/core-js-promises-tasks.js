c00210cc1138c1293e575675c92357c7bb45aa27
zyvy <gm.zyvy@gmail.com>
2024-04-05 21:22:11 +0300

    feat: started promises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,15 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const mypromise = new Promise((resolve, reject) => {
+    if (number > -1) {
+      resolve(number);
+    } else {
+      reject(number);
+    }
+  });
+  return mypromise;
 }
 
 /**
